f3da0c9aacdbfe439d273849afb86c55c5c6d354
Andre Sencioles <asenci@gmail.com>
2018-08-25 04:38:00 +1200

    Mention user on ping reply

--- ping.go
+++ ping.go
@@ -4,6 +4,6 @@ import "github.com/Necroforger/dgrouter/exrouter"
 
 // ToDo: chill bro
 func Ping(ctx *exrouter.Context) error {
-	ctx.Reply("pong")
+	ctx.Reply("<@", ctx.Msg.Author.ID, "> pong")
 	return nil
 }
